f3cb6d8ec931a0981a0c0fd984f06143deef287d
Neco86 <1157868866@qq.com>
2019-04-18 09:31:43 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { GlobalStyle } from './style.js'
+import Header from './common/header'
 
 function App (){
     return (
       <>
-        <div>hello world</div> 
+        <Header/>
         <GlobalStyle/>
       </>
     );
